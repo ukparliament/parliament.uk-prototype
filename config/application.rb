90be700078dcaffccc3a927683f53f23ebcd5d8e
giusepped <giusdesan@gmail.com>
2016-09-09 16:56:16 +0100

    added line in the config file to stop logs from outputting "cannot render console ..." errors

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ Bundler.require(*Rails.groups)
 
 module MembersPrototype
   class Application < Rails::Application
+
+    config.web_console.whiny_requests = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
