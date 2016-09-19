43b6d0cca4791e7673e59e53e7e8f7fddec05b32
Rebecca Appleyard <rklappleyard@gmail.com>
2016-09-19 12:16:01 +0100

    Completed tests for html response for index method

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -4,7 +4,6 @@ describe PeopleController do
   let(:json) { JSON.parse(response.body) }
   let(:xml) { Nokogiri::XML(response.body) }
   let(:ttl) { RDF::NTriples::Reader.new(response.body) }
-  let (:html) { response.body }
 
   describe "GET index" do
 
@@ -15,7 +14,8 @@ describe PeopleController do
             to_return(:status => 200, :body => PEOPLE_HASH.to_json, :headers => {})
         get 'index',format: :json
       end
-      it 'returns OK reponse with correct format' do
+
+      it 'returns OK response with correct format' do
         expect(response.status).to eq 200
         expect(response.content_type).to eq 'application/json'
       end
@@ -36,7 +36,8 @@ describe PeopleController do
             to_return(:status => 200, :body => PEOPLE_HASH.to_xml, :headers => {})
         get 'index',format: :xml
       end
-      it 'returns OK reponse with correct format' do
+
+      it 'returns OK response with correct format' do
         expect(response.status).to eq 200
         expect(response.content_type).to eq 'application/xml'
       end
@@ -55,7 +56,8 @@ describe PeopleController do
         allow(subject).to receive(:get_data).with('http://members-query.ukpds.org/people').and_return(PEOPLE_GRAPH)
         get 'index', format: :ttl
       end
-      it 'returns OK reponse with correct format' do
+
+      it 'returns OK response with correct format' do
         expect(response.status).to eq 200
         expect(response.content_type).to eq 'text/turtle'
       end
@@ -70,6 +72,24 @@ describe PeopleController do
     end
 
     context 'when the requested format is HTML' do
+      render_views
+
+      before(:each) do
+        stub_request(:get, "http://members-query.ukpds.org/people.json").
+            with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>'members-query.ukpds.org', 'User-Agent'=>'Ruby'}).
+            to_return(:status => 200, :body => PEOPLE_HASH.to_json, :headers => {})
+
+        get 'index', format: :html
+      end
+
+      it 'returns OK response with correct format' do
+        expect(response.status).to eq 200
+        expect(response.content_type).to eq 'text/html'
+      end
+
+      it 'returns the correct data for the first person' do
+        expect(response.body).to match(/<a href="\/people\/1">Member1<\/a>/)
+      end
     end
 
   end
@@ -83,7 +103,8 @@ describe PeopleController do
             to_return(:status => 200, :body => PERSON_ONE_HASH.to_json, :headers => {})
         get 'show', id: '1', format: :json
       end
-      it 'returns OK reponse with correct format' do
+
+      it 'returns OK response with correct format' do
         expect(response.status).to eq 200
         expect(response.content_type).to eq 'application/json'
       end
@@ -104,7 +125,8 @@ describe PeopleController do
             to_return(:status => 200, :body => PERSON_ONE_HASH.to_xml, :headers => {})
         get 'show', id: '1', format: :xml
       end
-      it 'returns OK reponse with correct format' do
+
+      it 'returns OK response with correct format' do
         expect(response.status).to eq 200
         expect(response.content_type).to eq 'application/xml'
       end
@@ -123,7 +145,8 @@ describe PeopleController do
         allow(subject).to receive(:get_data).with('http://members-query.ukpds.org/people/1').and_return(PERSON_ONE_GRAPH)
         get 'show', id: '1', format: :ttl
       end
-      it 'returns OK reponse with correct format' do
+
+      it 'returns OK response with correct format' do
         expect(response.status).to eq 200
         expect(response.content_type).to eq 'text/turtle'
       end
@@ -148,13 +171,14 @@ describe PeopleController do
         get 'show', id: '1', format: :html
       end
 
-      it 'returns OK reponse with correct format' do
+      it 'returns OK response with correct format' do
         expect(response.status).to eq 200
         expect(response.content_type).to eq 'text/html'
       end
 
       it 'returns the correct data for the person' do
         expect(response.body).to match(/<h1>Member1<\/h1>/)
+        expect(response.body).not_to have_content('Member2')
       end
     end
   end
