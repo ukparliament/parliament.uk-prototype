3a4cde3bc9c56239b84461e4f87bb03074a19939
Rebecca Appleyard <rklappleyard@gmail.com>
2016-09-20 13:28:51 +0100

    Re-instated stubbed requests for html requests as can now stub ttl request with a string rather than rdf graph

--- spec/feature_tests/person_detail_feature_spec.rb
+++ spec/feature_tests/person_detail_feature_spec.rb
@@ -3,15 +3,13 @@ require 'rails_helper'
 feature 'show page' do
   context 'when visiting a person detail page' do
     before(:each) do
-      # stub_request(:get, "http://members-query.ukpds.org/people/1.json").
-      #     with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>'members-query.ukpds.org', 'User-Agent'=>'Ruby'}).
-      #     to_return(:status => 200, :body => PERSON_ONE_HASH.to_json, :headers => {})
-      #
-      # stub_request(:get, "http://members-query.ukpds.org/people/1.ttl").
-      #     with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>'members-query.ukpds.org', 'User-Agent'=>'Ruby'}).
-      #     to_return(:status => 200, :body => "", :headers => {})
+      stub_request(:get, "#{MembersPrototype::Application.config.endpoint}/people/1.json").
+          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>"#{MembersPrototype::Application.config.endpoint_host}", 'User-Agent'=>'Ruby'}).
+          to_return(:status => 200, :body => PERSON_ONE_HASH.to_json, :headers => {})
 
-      allow_any_instance_of(PeopleController).to receive(:get_data).and_return({ graph: PERSON_ONE_TTL, json: PERSON_ONE_HASH.to_json })
+      stub_request(:get, "#{MembersPrototype::Application.config.endpoint}/people/1.ttl").
+          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>"#{MembersPrototype::Application.config.endpoint_host}", 'User-Agent'=>'Ruby'}).
+          to_return(:status => 200, :body => PERSON_ONE_TTL, :headers => {})
 
       visit person_path('1')
     end
