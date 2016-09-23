c5541d7d9eb1d791a96f9df1e80eb404a06e9f68
giusepped <giusdesan@gmail.com>
2016-09-23 14:59:52 +0100

    refactored method to create graph

--- app/controllers/concerns/query_helper.rb
+++ app/controllers/concerns/query_helper.rb
@@ -6,14 +6,16 @@ module QueryHelper
       format.html {
         json_request_uri = "#{uri}.json"
         ttl_request_uri = "#{uri}.ttl"
-        graph = query(ttl_request_uri)
+        ttl_data = query(ttl_request_uri)
+        graph = create_graph(ttl_data)
         json = query(json_request_uri)
 
         { graph: graph, json: json }
       }
       format.any(:xml, :json, :ttl) {
         request_uri = "#{uri}.#{request.format.to_sym.to_s}"
-        query(request_uri)
+        data = query(request_uri)
+        request.format.to_sym == :ttl ? create_graph(data) : data
       }
     end
   end
@@ -28,14 +30,20 @@ module QueryHelper
       format.any(:xml, :json) { render request.format.to_sym => data }
       format.ttl {
         result = ""
-        RDF::Reader.for(:ntriples).new(data) do |reader|
-          reader.each_statement do |statement|
-            result << RDF::NTriples::Writer.serialize(statement)
-          end
+        data.each_statement do |statement|
+          result << RDF::NTriples::Writer.serialize(statement)
         end
 
         render :text => result
       }
+    end
+  end
+
+  def create_graph(ttl_data)
+    RDF::NTriples::Reader.new(ttl_data) do |reader|
+      reader.each_statement do |statement|
+        RDF::Graph.new << statement
+      end
     end
   end
 
