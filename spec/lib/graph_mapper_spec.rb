5bd5e745eeeac3f1d8fe46aa0e38d356653f9434
Rebecca Appleyard <rklappleyard@gmail.com>
2016-11-08 12:47:55 +0000

    Fixed test stubs

--- spec/lib/graph_mapper_spec.rb
+++ spec/lib/graph_mapper_spec.rb
@@ -5,13 +5,19 @@ describe GraphMapper do
 
   describe '#create_graph_from_ttl' do
     it 'should create an RDF graph given ttl data in a string format' do
-      expect(extended_class.create_graph_from_ttl(PERSON_ONE_TTL).first).to eq PEOPLE_STATEMENTS[0]
+      expect(extended_class.create_graph_from_ttl(PERSON_ONE_TTL).first).to eq PEOPLE_GRAPH.first
     end
   end
 
   describe '#to_underscore_case' do
     it 'should transform dateOfBirth to date_of_birth' do
       expect(extended_class.to_underscore_case('dateOfBirth')).to eq 'date_of_birth'
+    end
+  end
+
+  describe '#get_id' do
+    it 'should return the id given a uri' do
+      expect(extended_class.get_id(RDF::URI.new('http://id.ukpds.org/123'))).to eq '123'
     end
   end
 end
