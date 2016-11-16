5c3df98d097e14624b6774c716e8dc673338c1fe
giusepped <giusdesan@gmail.com>
2016-11-16 13:07:42 +0000

    showing parties with start dates for party_memberships in the show page for a member

--- lib/grom/base.rb
+++ lib/grom/base.rb
@@ -31,7 +31,7 @@ module Grom
 
     def self.has_many_through(association, through_association)
       self.has_many(through_association)
-      self.class_eval("def #{association}; #{singularize(association.to_s).capitalize}.has_many_through_query(self, #{through_association.to_s.chop.capitalize}.new({}).class.name); end")
+      self.class_eval("def #{association}; #{camelize(singularize(association.to_s).capitalize)}.has_many_through_query(self, #{camelize(through_association.to_s.chop.capitalize)}.new({}).class.name); end")
     end
 
     def self.has_many_query(owner_object, optional=nil)
@@ -45,7 +45,7 @@ module Grom
       graph_data = get_graph_data(endpoint_url)
       separated_graphs = split_by_subject(graph_data, self.name)
       associated_objects_array = self.all(separated_graphs[:associated_class_graph])
-      through_property_plural = pluralize(through_class.downcase)
+      through_property_plural = pluralize(underscore(through_class).downcase)
       self.class_eval("def #{through_property_plural}=(array); @#{through_property_plural} = array; end")
       self.class_eval("def #{through_property_plural}; @#{through_property_plural}; end")
       associated_objects_array.each do |associated_object|
