4681799a165ed98bf352d2eb5416dac134230544
Rebecca Appleyard <rklappleyard@gmail.com>
2016-11-21 16:46:32 +0000

    Added members for parties

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -19,7 +19,7 @@ class PartiesController < ApplicationController
   end
 
   def members
-    endpoint_url = "#{API_ENDPOINT}/parties/#{params[:person_id]}/members.ttl"
+    endpoint_url = "#{API_ENDPOINT}/parties/#{params[:party_id]}.ttl"
     result = get_graph_data(endpoint_url)
     party = Party.find(result)
     @members = party.members
