d1c4abfcfa5eaf84089faa253458df0878492626
Rebecca Appleyard <rklappleyard@gmail.com>
2016-12-02 09:14:09 +0000

    Removed graph from contact points controller

--- app/controllers/contact_points_controller.rb
+++ app/controllers/contact_points_controller.rb
@@ -2,17 +2,13 @@ class ContactPointsController < ApplicationController
 
   def index
     @contact_points = ContactPoint.all
-    graph = ContactPoint.collective_graph(@contact_points)
-    @json_ld = json_ld(graph)
 
-    format({ serialized_data: @contact_points, graph_data: graph })
+    format({ serialized_data: @contact_points })
   end
 
   def show
     @contact_point = ContactPoint.find(params[:id])
-    graph = @contact_point.graph
-    @json_ld = json_ld(graph)
 
-    format({ serialized_data: @contact_point, graph_data: graph })
+    format({ serialized_data: @contact_point })
   end
 end
