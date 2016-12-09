03cdac8a4c43200f37e0b318b25cff8f7c307b84
Rebecca Appleyard <rklappleyard@gmail.com>
2016-12-09 14:00:32 +0000

    Finished testing for parties_controller,contact_points_controller and most of people_controller

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -1,128 +1,243 @@
 require 'rails_helper'
-require 'json'
 
 RSpec.describe PeopleController do
-  let(:json) { JSON.parse(response.body) }
-  let(:xml) { Nokogiri::XML(response.body) }
-  let(:ttl) { RDF::NTriples::Reader.new(response.body) }
 
   describe "GET index" do
-    it 'the response should have http status ok (200)' do
+    before(:each) do
+      get :index
+    end
+
+    it 'should have a response with http status ok (200)' do
       expect(response).to have_http_status(:ok)
     end
 
     it 'assigns @people' do
-      get :index
-      expect(assigns(:people)[0]).to be_a(Person)
+      assigns(:people).each do |person|
+        expect(person).to be_a(Person)
+      end
     end
 
-    it 'assigns @people in name order' do
-      get :index
+    it 'assigns @people in alphabetical order' do
       expect(assigns(:people)[0].forename).to eq("Arya")
       expect(assigns(:people)[1].forename).to eq("Daenerys")
     end
 
-    it 'renders the show template' do
-      get :index
+    it 'renders the index template' do
       expect(response).to render_template('index')
     end
   end
 
   xdescribe "GET show" do
-    it 'assigns @peron' do
+    before(:each) do
       get :show, params: { id: '1' }
+    end
+
+    it 'should have a response with http status ok (200)' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'assigns @peron' do
       expect(assigns(:person)).to be_a(Person)
     end
 
-    it 'renders the index template' do
-      get :show, params: { id: '1' }
+    it 'renders the show template' do
       expect(response).to render_template('show')
     end
   end
 
   describe "GET members" do
-    it 'assigns @people' do
+    before(:each) do
       get :members
-      expect(assigns(:people)[0]).to be_a(Person)
     end
 
-    it 'assigns @people in name order' do
-      get :members
+    it 'should have a response with http status ok (200)' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'assigns @people' do
+      assigns(:people).each do |person|
+        expect(person).to be_a(Person)
+      end
+    end
+
+    it 'assigns @people in alphabetical order' do
       expect(assigns(:people)[0].forename).to eq("Arya")
       expect(assigns(:people)[1].forename).to eq("Daenerys")
     end
 
     it 'renders the members template' do
-      get :members
       expect(response).to render_template('members')
     end
   end
 
   describe "GET current_members" do
-    it 'assigns @people' do
+    before(:each) do
       get :current_members
-      expect(assigns(:people)[0]).to be_a(Person)
     end
 
-    it 'assigns @people in name order' do
-      get :current_members
+    it 'should have a response with http status ok (200)' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'assigns @people' do
+      assigns(:people).each do |person|
+        expect(person).to be_a(Person)
+      end
+    end
+
+    it 'assigns @people in alphabetical order' do
       expect(assigns(:people)[0].forename).to eq("Arya")
       expect(assigns(:people)[1].forename).to eq("Daenerys")
     end
 
-    it 'renders the members template' do
-      get :current_members
+    it 'renders the current_members template' do
       expect(response).to render_template('current_members')
     end
   end
 
-  describe "GET letters" do
-    it 'assigns @people' do
-      get :letters, params: { letter: "t" }
-      expect(assigns(:people)[0]).to be_a(Person)
+  describe "GET contact_points" do
+    before(:each) do
+      get :contact_points, params: { person_id: '1' }
     end
 
-    it 'assigns @people in name order' do
+    it 'should have a response with http status ok (200)' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'assigns @person and @contact_points' do
+      expect(assigns(:person)).to be_a(Person)
+
+      assigns(:contact_points).each do |cp|
+        expect(cp).to be_a(ContactPoint)
+      end
+    end
+
+    it 'renders the contact_points template' do
+      expect(response).to render_template('contact_points')
+    end
+  end
+
+  describe "GET parties" do
+    before(:each) do
+      get :parties, params: { person_id: '1' }
+    end
+
+    it 'should have a response with http status ok (200)' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'assigns @person and @parties' do
+      expect(assigns(:person)).to be_a(Person)
+
+      assigns(:parties).each do |party|
+        expect(party).to be_a(Party)
+      end
+    end
+
+    it 'assigns @parties in alphabetical order' do
+      expect(assigns(:parties)[0].name).to eq("Starks")
+      expect(assigns(:parties)[1].name).to eq("Targaryens")
+    end
+
+    it 'renders the parties template' do
+      expect(response).to render_template('parties')
+    end
+  end
+
+  describe "GET current_parties" do
+    before(:each) do
+      get :current_parties, params: { person_id: '1' }
+    end
+
+    it 'should have a response with http status ok (200)' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'assigns @person and @parties' do
+      expect(assigns(:person)).to be_a(Person)
+
+      assigns(:parties).each do |party|
+        expect(party).to be_a(Party)
+      end
+    end
+
+    it 'assigns @parties in alphabetical order' do
+      expect(assigns(:parties)[0].name).to eq("Starks")
+    end
+
+    it 'renders the current_parties template' do
+      expect(response).to render_template('current_parties')
+    end
+  end
+
+  describe "GET letters" do
+    before(:each) do
       get :letters, params: { letter: "t" }
+    end
+
+    it 'should have a response with http status ok (200)' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'assigns @people' do
+      assigns(:people).each do |person|
+        expect(person).to be_a(Person)
+      end
+    end
+
+    it 'assigns @people in alphabetical order' do
       expect(assigns(:people)[0].forename).to eq("Daenerys")
     end
 
-    it 'renders the members template' do
-      get :letters, params: { letter: "t" }
+    it 'renders the index template' do
       expect(response).to render_template('index')
     end
   end
 
   describe "GET members_letters" do
-    it 'assigns @people' do
+    before(:each) do
       get :members_letters, params: { letter: "t" }
-      expect(assigns(:people)[0]).to be_a(Person)
     end
 
-    it 'assigns @people in name order' do
-      get :members_letters, params: { letter: "t" }
+    it 'should have a response with http status ok (200)' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'assigns @people' do
+      assigns(:people).each do |person|
+        expect(person).to be_a(Person)
+      end
+    end
+
+    it 'assigns @people in alphabetical order' do
       expect(assigns(:people)[0].forename).to eq("Daenerys")
     end
 
     it 'renders the members template' do
-      get :members_letters, params: { letter: "t" }
       expect(response).to render_template('members')
     end
   end
 
   describe "GET current_members_letters" do
-    it 'assigns @people' do
+    before(:each) do
       get :current_members_letters, params: { letter: "t" }
-      expect(assigns(:people)[0]).to be_a(Person)
     end
 
-    it 'assigns @people in name order' do
-      get :current_members_letters, params: { letter: "t" }
+    it 'should have a response with http status ok (200)' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'assigns @people' do
+      assigns(:people).each do |person|
+        expect(person).to be_a(Person)
+      end
+    end
+
+    it 'assigns @people in alphabetical order' do
       expect(assigns(:people)[0].forename).to eq("Daenerys")
     end
 
     it 'renders the members template' do
-      get :current_members_letters, params: { letter: "t" }
       expect(response).to render_template('members')
     end
   end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -77,6 +77,59 @@ RSpec.configure do |config|
       stub_request(:get, "#{API_ENDPOINT}/people/members/current/t.ttl").
           with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>"#{API_ENDPOINT_HOST}", 'User-Agent'=>'Ruby'}).
           to_return(:status => 200, :body => PERSON_ONE_TTL, :headers => {})
+
+      stub_request(:get, "#{API_ENDPOINT}/people/1/contact_points.ttl").
+          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>"#{API_ENDPOINT_HOST}", 'User-Agent'=>'Ruby'}).
+          to_return(:status => 200, :body => CONTACT_POINT_TTL, :headers => {})
+
+      stub_request(:get, "#{API_ENDPOINT}/contact_points/123.ttl").
+          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>"#{API_ENDPOINT_HOST}", 'User-Agent'=>'Ruby'}).
+          to_return(:status => 200, :body => CONTACT_POINT_TTL, :headers => {})
+
+      stub_request(:get, "#{API_ENDPOINT}/contact_points.ttl").
+          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>"#{API_ENDPOINT_HOST}", 'User-Agent'=>'Ruby'}).
+          to_return(:status => 200, :body => TWO_CONTACT_POINTS_TTL, :headers => {})
+
+      stub_request(:get, "#{API_ENDPOINT}/people/1/parties.ttl").
+          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>"#{API_ENDPOINT_HOST}", 'User-Agent'=>'Ruby'}).
+          to_return(:status => 200, :body => TWO_PARTIES_TTL, :headers => {})
+
+      stub_request(:get, "#{API_ENDPOINT}/parties.ttl").
+          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>"#{API_ENDPOINT_HOST}", 'User-Agent'=>'Ruby'}).
+          to_return(:status => 200, :body => TWO_PARTIES_TTL, :headers => {})
+
+      stub_request(:get, "#{API_ENDPOINT}/parties/81.ttl").
+          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>"#{API_ENDPOINT_HOST}", 'User-Agent'=>'Ruby'}).
+          to_return(:status => 200, :body => PARTY_TTL, :headers => {})
+
+      stub_request(:get, "#{API_ENDPOINT}/people/1/parties/current.ttl").
+          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>"#{API_ENDPOINT_HOST}", 'User-Agent'=>'Ruby'}).
+          to_return(:status => 200, :body => PARTY_TTL, :headers => {})
+
+      stub_request(:get, "#{API_ENDPOINT}/parties/current.ttl").
+          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>"#{API_ENDPOINT_HOST}", 'User-Agent'=>'Ruby'}).
+          to_return(:status => 200, :body => TWO_PARTIES_TTL, :headers => {})
+
+      stub_request(:get, "#{API_ENDPOINT}/parties/s.ttl").
+          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>"#{API_ENDPOINT_HOST}", 'User-Agent'=>'Ruby'}).
+          to_return(:status => 200, :body => PARTY_TTL, :headers => {})
+
+      stub_request(:get, "#{API_ENDPOINT}/parties/81/members.ttl").
+          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>"#{API_ENDPOINT_HOST}", 'User-Agent'=>'Ruby'}).
+          to_return(:status => 200, :body => PEOPLE_TTL, :headers => {})
+
+      stub_request(:get, "#{API_ENDPOINT}/parties/81/members/current.ttl").
+          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>"#{API_ENDPOINT_HOST}", 'User-Agent'=>'Ruby'}).
+          to_return(:status => 200, :body => PEOPLE_TTL, :headers => {})
+
+      stub_request(:get, "http://ukparliament-graph-api.herokuapp.com/parties/81/members/t.ttl").
+          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>'ukparliament-graph-api.herokuapp.com', 'User-Agent'=>'Ruby'}).
+          to_return(:status => 200, :body => PERSON_ONE_TTL, :headers => {})
+
+      stub_request(:get, "http://ukparliament-graph-api.herokuapp.com/parties/81/members/current/t.ttl").
+          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>'ukparliament-graph-api.herokuapp.com', 'User-Agent'=>'Ruby'}).
+          to_return(:status => 200, :body => PERSON_ONE_TTL, :headers => {})
+
     end
   end
 end
