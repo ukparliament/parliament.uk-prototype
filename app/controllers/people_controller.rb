6a4575a5ba8b9b1e2e94f154761a0db43642df35
Rebecca Appleyard <rklappleyard@gmail.com>
2017-01-06 16:46:40 +0000

    Updated people/contact_points to use eager_find

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -29,7 +29,6 @@ class PeopleController < ApplicationController
 
   def contact_points
     @person = Person.eager_find(params[:person_id]) or not_found
-    # @contact_points = @person.contact_points
 
     format({ serialized_data: @person })
   end
