d404bd9cfffe8600e63be6ec91d8bb8e23a34e2f
ALLETT <alletts@ALLETTs-MacBook-Pro.local>
2017-02-28 16:37:05 +0000

    Dummy change

--- aws_ecs/register-task-definition.sh
+++ aws_ecs/register-task-definition.sh
@@ -4,3 +4,4 @@ TMPFILE=$(mktemp)
 aws ecs describe-task-definition --task-definition memberserviceapi | jq "{ containerDefinitions: .taskDefinition.containerDefinitions, family: .taskDefinition.family }" > $TMPFILE
 aws ecs register-task-definition --cli-input-json file://$TMPFILE
 rm $TMPFILE
+
