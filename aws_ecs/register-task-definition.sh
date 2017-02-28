77a936ebb520f559604f647221f4365ad01137aa
ALLETT <alletts@ALLETTs-MacBook-Pro.local>
2017-02-28 09:26:36 +0000

    Changes for task definition. Removing json and editing makefile

--- aws_ecs/register-task-definition.sh
+++ aws_ecs/register-task-definition.sh
@@ -1,10 +1,6 @@
 #! /usr/bin/env bash
 
-IMAGE=$1
-APP_NAME=$2
-AWS_REGION=$3
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TMPFILE=$(mktemp)
-cat $DIR/task-definition.json | sed -e s#\{\{IMAGE\}\}#$IMAGE# -e s#\{\{APP_NAME\}\}#$APP_NAME# -e s#\{\{AWS_REGION\}\}#$AWS_REGION# > $TMPFILE
+aws ecs describe-task-definition --task-definition memberserviceapi | jq "{ containerDefinitions: .taskDefinition.containerDefinitions, family: .taskDefinition.family }" > $TMPFILE
 aws ecs register-task-definition --cli-input-json file://$TMPFILE
 rm $TMPFILE
