04f7033b930978df433573af8a74460e90469440
EBIRIM <ebirima@EBIRIMs-MacBook-Pro.local>
2017-03-09 19:39:41 +0000

    * Addition of helper for dynamic guids * Addition of test for dynamic guids in application controller spec * Addition o=f dynamic guids to /index view for home controller * Addition of dynamic guids to current members, members, current_members_letters and members_letters views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 require 'vcard/vcard'
 require 'parliament'
+require 'houses_id_helper'
 
 class ApplicationController < ActionController::Base
   include VCardHelper
   include Parliament
+  include HouseIdHelper
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
