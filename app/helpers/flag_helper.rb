42d3e960b86dcb986d564047643895f39bd68811
cwaszczuk <waszczukc@parliament.uk>
2017-05-04 17:13:21 +0100

    [WEBSITE-1098] Added new helper to flag helper class (#405)

--- app/helpers/flag_helper.rb
+++ app/helpers/flag_helper.rb
@@ -2,4 +2,8 @@ module FlagHelper
   def self.dissolution?
     BANDIERA_CLIENT.enabled?('parliament', 'show-dissolution')
   end
+
+  def self.register_to_vote?
+    BANDIERA_CLIENT.enabled?('parliament', 'show-register')
+  end
 end
