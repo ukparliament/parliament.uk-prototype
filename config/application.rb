4d021132c7f0ad2db2104023144d6458d864cf77
jennaramdenee <jenna_ramdenee@hotmail.co.uk>
2017-05-22 16:53:24 +0100

    [WEBSITE-1020] Remove trailing slashes from URL
    * Created feature spec folder
    * Added application_feature_spec file with trailing slash tests
    * Added 'rack-rewrite' gem to Gemfile
    * Added middleware configuration to config.ru in order to remove trailing slash from URI
    * Required Capybara/RSpec to spec_helper

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ require 'action_controller/railtie'
 require 'action_mailer/railtie'
 require 'action_view/railtie'
 require 'sprockets/railtie'
+require 'rack/rewrite'
 # require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
@@ -17,6 +18,12 @@ Bundler.require(*Rails.groups)
 
 module MembersPrototype
   class Application < Rails::Application
+
+    # Rewrite trailing slashes
+    config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
+      r301 %r{^/(.*)/$}, '/$1'
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'simplecov'
 SimpleCov.start 'rails'
 
 require 'webmock/rspec'
+require 'capybara/rspec'
 
 WebMock.disable_net_connect!(allow_localhost: true)
 
